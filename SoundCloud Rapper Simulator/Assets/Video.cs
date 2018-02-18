84472e6f9833161d9ea3393cbe2c21257fcc601f
Liam Gensel <liamgens@gmail.com>
2018-02-17 23:11:28 -0500

    added video stuff

--- SoundCloud Rapper Simulator/Assets/Video.cs
+++ SoundCloud Rapper Simulator/Assets/Video.cs
@@ -5,12 +5,16 @@ using UnityEngine;
 public class Video
 {
     private Song song;
+	private int views;
 
     public Song Song { get { return song;  } }
+	public string Title { get { return song.Title; } }
+	public int Views { get { return views; } }
 
     public Video(Song song)
     {
         this.song = song;
+		this.views = 0;
     }
 
 
